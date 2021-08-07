a0ac782ff9d4830fe7419f13fa057ffb4e2fb8d7
szabogtamas <szabogtamas@gmail.com>
2021-08-07 16:40:26 +0200

    Created script to run similarity scoring in R

---
+++ scripts/similarity_r/calculate_jaccard.R
@@ -0,0 +1,134 @@
+#!/usr/bin/env Rscript
+
+############################################################################
+#                                                                          #
+#   Calculates Jaccard similarity of entities with categorical labels.     #
+#   Preferred input is long format data, but it is possible to adapt.      #
+#                                                                          #
+############################################################################
+
+scriptDescription <- "Calculate Jaccard similarity of labelled entities."
+
+scriptMandatoryArgs <- list(
+  inFile = list(
+    abbr="-i",
+    help="Path to a table with input data."
+  )
+)
+
+scriptOptionalArgs <- list(
+  inFormat = list(
+    default="long",
+    help="Format of data in the input. Possible values are [long, tabular, nested, binary]."
+  ),
+  outPath = list(
+    default="jaccard_similarity.csv",
+    help="Path to output table capturing similarities."
+  )
+)
+
+for (pk in c("tidyr", "dplyr")){
+  if(!(pk %in% (.packages()))){
+    library(pk, character.only=TRUE)
+  }
+}
+
+#' The main function of the script, executed only if called from command line.
+#' Calls subfunctions according to supplied command line arguments.
+#' 
+#' @param opt list. a named list of all command line options; will be passed on 
+#' 
+#' @return Not intended to return anything, but rather to save outputs to files.
+main <- function(opt){
+  
+  cat("Standardizing input\n")
+  main_mat <- standardize_input_data(opt$inFile, opt$inFormat)
+  
+  cat("Calculating similarity\n")
+  jaccard_sims <- calculate_jaccard(main_mat)
+
+  cat("Saving similarity matrix\n")
+  write.csv(jaccard_sims, opt$outPath)
+
+  invisible(NULL)
+}
+
+
+
+#' Remove custom argument features that are not understood by arg_parse.
+#' 
+#' @param parser object.  An arg_parse parser instance.
+#' @param arg_defs list.  Argument definitions at the beginning of the script.
+#' 
+#' @return list.
+parser4arglist <- function(parser, arg_defs){
+  for (al in arg_defs){
+    for (rgn in names(al)){
+      rg <- al[[rgn]]
+      rga <- paste0("--", rgn)
+      if ("abbr" %in% names(rg) ) {
+        rga <- c(rg[["abbr"]], rga)
+        rg[["abbr"]] <- NULL
+      }
+      rl <- list(parser, rga)
+      rl <- c(rl, rg)
+      parser <- do.call(add_option, rl)
+    }
+  }
+  invisible(parser)
+}
+
+####    Intarfacing with command line below, nothing related to the main functionality    ####
+
+if (!interactive()) {
+  
+  # Initialize parser with verbosity and description of script
+  parser <- OptionParser(usage=paste0("%prog [options]\nDescription:\n  ", scriptDescription))
+  parser <- add_option(
+    parser,
+    c("-v", "--verbose"),
+    action="store_true",
+    default=FALSE,
+    help="Print some progress messages to stdout."
+    )
+  parser <- add_option(
+    parser,
+    c("-q", "--quietly"),
+    action="store_false",
+    dest="verbose",
+    help="Create figures quietly, without printing to stdout."
+  )
+  
+  # Add custom arguments to parser
+  for (arg_def in list(scriptMandatoryArgs, scriptOptionalArgs)){
+    for (arg_name in names(arg_def)){
+      rg <- al[[arg_name]]
+      arg_alias <- paste0("--", arg_name)
+      if ("abbr" %in% names(rg) ) {
+        arg_alias <- c(rg[["abbr"]], arg_alias)
+        rg[["abbr"]] <- NULL
+      }
+      parser <- do.call(add_option, c(list(parser, arg_alias), rg))
+    }
+  }
+  opt <- parse_args(parser)
+
+  # Check if mandatory arguments are present
+  passed_args <- opt[names(scriptMandatoryArgs)]
+  if (any(is.na(names(passed_args)))) {
+    if (opt$verbose) { 
+      write("Sorry, cannot proceed without all mandatory arguments.\n", stderr())
+    }
+    checkpass <- FALSE
+  } else {
+    checkpass <- TRUE
+  }
+
+  # Execute main function if mandatory arguments are set (otherwise print help message)
+  if (checkpass) { 
+    main(opt)
+  } else {
+    print_help(parser)
+  }
+  
+}
